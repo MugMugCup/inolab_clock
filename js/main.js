709f5571918eba756397b9f06b8be05dc3ac2a8e
MugMugCup <139062914+MugMugCup@users.noreply.github.com>
2024-07-09 19:40:00 +0900

    add dead_line

--- js/main.js
+++ js/main.js
@@ -6,15 +6,25 @@ function displayDateTime() {
     var day = ('0' + now.getDate()).slice(-2);
     var weeks = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
     var week = weeks[now.getDay()]
-    var hours = ('0' + now.getHours()).slice(-2);
-    var minutes = ('0' + now.getMinutes()).slice(-2);
-    var seconds = ('0' + now.getSeconds()).slice(-2);
+    var hour = ('0' + now.getHours()).slice(-2);
+    var minute = ('0' + now.getMinutes()).slice(-2);
+    var second = ('0' + now.getSeconds()).slice(-2);
+    
+    // デッドラインを計算
+    const target_date = new Date("2024-07-11T23:59:59").getTime();
+    var distance = target_date - now;
+    var dead_day = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var dead_hour = Math.floor(distance % ((1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var dead_minute = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var dead_second = Math.floor((distance % (1000 * 60)) / 1000);
 
     // HTML に出力
-    var time = hours + ':' + minutes + ':' + seconds
+    var time = hour + ':' + minute + ':' + second;
     document.getElementById('time').innerHTML = time;
-    var data = year + '/' + month + '/' + day + ' (' + week + ')';
-    document.getElementById('data').innerHTML = data;
+    var date = year + '/' + month + '/' + day + ' (' + week + ')';
+    document.getElementById('date').innerHTML = date;
+    var dead_date = dead_day + 'D:' + dead_hour + 'h:' + dead_minute + 'm:' + dead_second + 's';
+    document.getElementById('dead_date').innerHTML = dead_date;
 }
 
 window.onload = function () {
